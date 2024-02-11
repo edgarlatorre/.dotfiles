5283268549628c314e9ca92d1e7a5cb6c17bf0e6
Edgar Latorre <me@edgarlatorre.com.br>
2024-02-11 22:28:03 +0100

    Remove signs from gitsigns

--- nvim/.config/nvim/lua/edgar/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/edgar/plugins/gitsigns.lua
@@ -1,13 +1,6 @@
 return {
   "lewis6991/gitsigns.nvim",
   opts = {
-    signs = {
-      add = { text = "+" },
-      change = { text = "~" },
-      delete = { text = "_" },
-      topdelete = { text = "‾" },
-      changedelete = { text = "~" },
-    },
     current_line_blame = true,
     on_attach = function(bufnr)
       vim.keymap.set(
