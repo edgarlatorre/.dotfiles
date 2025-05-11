cebf439895aaf4e70fe9bbe43c7c3ce3f7a97ba7
Edgar Latorre <me@edgarlatorre.com.br>
2025-05-11 11:06:38 +0200

    Add atuin and zoxide init and avante

--- zsh/.zsh_files/init.zsh
+++ zsh/.zsh_files/init.zsh
@@ -2,6 +2,8 @@ export LC_ALL=en_US.UTF-8
 
 eval "$(starship init zsh)"
 eval "$(/usr/bin/mise activate zsh)"
+eval "$(atuin init zsh --disable-up-arrow)"
+eval "$(zoxide init zsh)"
 
 if command -v fzf &> /dev/null; then
   [ -r /usr/share/bash-completion/completions/fzf ] && . /usr/share/bash-completion/completions/fzf
