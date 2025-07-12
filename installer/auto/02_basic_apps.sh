721b32ab4d1d406b5cd18166772f519fe8b2b0b0
Edgar Latorre <me@edgarlatorre.com.br>
2025-07-12 07:41:09 +0200

    Improve dunst and install plymouth

--- installer/auto/02_basic_apps.sh
+++ installer/auto/02_basic_apps.sh
@@ -1,1 +1,1 @@
-yay -S --needed swayosd fzf ripgrep bat eza zoxide plocate btop fd tldr neovim alacritty lazygit lazydocker starship mise hyprpaper hyprlock wlogout nemo imv  --noconfirm
+yay -S --needed swayosd fzf ripgrep bat eza zoxide plocate btop fd tldr neovim alacritty lazygit lazydocker starship mise hyprpaper hyprlock wlogout nemo imv nwg-look plymouth --noconfirm
